32f2c7250147726e30d79633b57b394958710b76
Khaled Ahmed <>
2021-06-09 14:52:30 +0200

    Solve bug

--- app/controllers/fat_free_crm/contacts_controller.rb
+++ app/controllers/fat_free_crm/contacts_controller.rb
@@ -38,7 +38,10 @@ class ContactsController < FatFreeCrm::EntitiesController
 
     if params[:related]
       model, id = params[:related].split('_')
-      if related = model.classify.constantize.my(current_user).find_by_id(id)
+      if model == "account"
+        related = FatFreeCrm::Account.my(current_user).find_by_id(id)
+      elsif related = model.classify.constantize.my(current_user).find_by_id(id)
+      # if related = FatFreeCrm::Account.my(current_user).find_by_id(id)
         instance_variable_set("@#{model}", related)
       else
         respond_to_related_not_found(model) && return
