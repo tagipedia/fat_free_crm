25c6363a56463b76e392ef9d0d00511d33bdd1aa
Shimaa Hassan <shimaa.hassan@tagipedia.com>
2021-06-10 09:26:51 +0200

    redefine field_groups

--- db/migrate/20150123060900_convert_radio_to_radio_buttons.rb
+++ db/migrate/20150123060900_convert_radio_to_radio_buttons.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class ConvertRadioToRadioButtons < ActiveRecord::Migration[4.2]
+  module SingletonMethods
+    def field_groups
+      FatFreeCrm::Field.table_name = 'fields'
+      FatFreeCrm::FieldGroup.table_name = 'field_groups'
+      if ActiveRecord::Base.connection.data_source_exists? 'field_groups'
+        FatFreeCrm::FieldGroup.where(klass_name: name).order(:position)
+      else
+        []
+      end
+    end
+  end
   def up
     # UPDATE "fields" SET "as" = 'radio_buttons' WHERE "fields"."as" = $1  [["as", "radio"]]
     FatFreeCrm::Field.where(as: 'radio').update_all(as: 'radio_buttons')
--- lib/fat_free_crm/fields.rb
+++ lib/fat_free_crm/fields.rb
@@ -24,8 +24,6 @@ module FatFreeCrm
 
     module SingletonMethods
       def field_groups
-        # FatFreeCrm::Field.table_name = 'fields'
-        # FatFreeCrm::FieldGroup.table_name = 'field_groups'
         if ActiveRecord::Base.connection.data_source_exists? 'fat_free_crm_field_groups'
           FatFreeCrm::FieldGroup.where(klass_name: name).order(:position)
         else
