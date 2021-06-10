5cbf879478a9390eb35a8df26c7cdec2d2ce3001
Shimaa Hassan <shimaa.hassan@tagipedia.com>
2021-06-10 09:17:42 +0200

    redefine Tag and Tagging class

--- db/migrate/20141230205454_add_taggings_counter_cache_to_tags.acts_as_taggable_on_engine.rb
+++ db/migrate/20141230205454_add_taggings_counter_cache_to_tags.acts_as_taggable_on_engine.rb
@@ -2,6 +2,16 @@
 
 # This migration comes from acts_as_taggable_on_engine (originally 3)
 class AddTaggingsCounterCacheToTags < ActiveRecord::Migration[4.2]
+  module ActsAsTaggableOn
+    class Tag < ::ActiveRecord::Base
+      self.table_name = "tags"
+    end
+
+    class Tagging < ::ActiveRecord::Base
+      self.table_name = "taggings"
+    end
+  end
+  
   def self.up
     add_column :tags, :taggings_count, :integer, default: 0
 
