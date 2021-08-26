3aa766c8d5b38e4e29d1253a433de7de82b1a006
Khaled Ahmed <khaled.ahmed@tagipedia.com>
2021-08-26 13:19:56 +0200

    changes

--- app/controllers/fat_free_crm/email_designs_controller.rb
+++ app/controllers/fat_free_crm/email_designs_controller.rb
@@ -68,9 +68,8 @@ class EmailDesignsController < FatFreeCrm::EntitiesController
   # DELETE /email_designs/1
   #----------------------------------------------------------------------------
   def destroy
-    @email_design.campaign.email_designs.delete(@email_design.id)
-    # @email_design.destroy
-    update_sidebar
+    @email_design.destroy
+
     respond_with(@email_design) do |format|
       format.html { respond_to_destroy(:html) }
       format.js   { respond_to_destroy(:ajax) }
