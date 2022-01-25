0493dc2c216f4c5f4b11df2631736c85286ce783
Shimaa Hassan <shimaa.hassan@tagipedia.com>
2022-01-25 14:38:00 +0200

    override same as https://github.com/activerecord-hackery/ransack/blob/v2.4.2/lib/ransack/nodes/attribute.rb

---
+++ lib/ransack_ui/ransack_ui/ransack_overrides/nodes/attribute.rb
@@ -0,0 +1,15 @@
+require 'ransack/nodes/attribute'
+
+module Ransack
+  module Nodes
+    Attribute.class_eval do
+      def valid?
+        bound? && attr &&
+        (context.klassify(parent).ransackable_attributes(context.auth_object)
+        .include?(attr_name.split('.').last) ||
+        context.klassify(parent).ransackable_attributes(context.auth_object)
+        .map(&:first).include?(attr_name))
+      end
+    end
+  end
+end
